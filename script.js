2ebaf5b05a786b3682493ca3e681e254b664811d
Subham Saha <subhamranjansaha006@gmail.com>
2026-05-14 01:03:16 +0530

    Added the second page with goal and maintenance

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function start(btn)
     let weight=+document.getElementById("weight").value;
     let activity=document.getElementById("activity").value;
     let goal=btn.textContent;
-    console.log(goal);
+    let bmr;
+    if(gender=="Male")
+        bmr= (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    else
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161; 
+    let maintenance = bmr * activity;
+    document.getElementById("container").style.display="none";
+    document.getElementById("second-container").style.display="flex";
+
+    document.getElementById("goal").textContent= "Goal Chosen : " + goal; 
+    document.getElementById("maintenance").textContent= "Maintenance calories : "+Math.round(maintenance) + " calories";
+    
 
 }
