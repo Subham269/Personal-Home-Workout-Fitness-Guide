3d3cd04f1b35afc3cf71ed882e2213fd7f9e0016
Subham Saha <subhamranjansaha006@gmail.com>
2026-05-13 15:08:09 +0530

    Added some backend inputs

---
+++ script.js
@@ -0,0 +1,11 @@
+function start(btn)
+{
+    let age=+document.getElementById("age").value;
+    let gender=(document.getElementById("Gender").value);
+    let height=Number(document.getElementById("height").value);
+    let weight=+document.getElementById("weight").value;
+    let activity=document.getElementById("activity").value;
+    let goal=btn.textContent;
+    console.log(goal);
+
+}
